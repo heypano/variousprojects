461f51e3a2bba66f0a050bd10468a2cd9c78e4c8
Pano Papadatos <ppapadatos@sourcefire.com>
2013-10-21 11:47:55 -0400

    node js chat

--- node-v0.10.21/app.js
+++ node-v0.10.21/app.js
@@ -1,26 +1,40 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs')
+var app = require('http').createServer(handler);
+var io = require('socket.io').listen(app);
+var fs = require('fs');
 
 app.listen(80);
-
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
+function handler(req, res) {
+	fs.readFile(__dirname + req.url, function(err, data) {
+		if (err) {
+			res.writeHead(500);
+			return res.end('Error loading '+req.url);
+		}
+		
+		res.writeHead(200);
+		res.end(data);
+	});
 }
 
-io.sockets.on('connection', function (socket) {
-	socket.emit('news', { hello: 'world' });
-  socket.on('message', function (message) {
-	  socket.emit('news', { hello: 'world' });
-    socket.emit('message', {"message":message});
-  });
+
+
+
+io.sockets.on('connection', function(socket) {
+	/*socket.emit('news', {
+		hello : 'world'
+	});*/
+	
+	socket.on('message', function(data) {
+		var name = data.name;
+		var message = data.message;
+		// Send the message to everyone else
+		socket.broadcast.emit('message', {
+			"name" : name,
+			"message" : message
+		});
+		// Send the message back to the person
+		socket.emit('message', {
+			"name" : name,
+			"message" : message
+		});
+	});
 });
